db7ec125f693bd5ec7474b7e86c1f803d597b427
vena202020 <113119673+vena-stack@users.noreply.github.com>
2025-06-25 03:03:24 +0700

    soon gonna update 4z0t mod with more fun things

---
+++ InstantAssist/mod_info.lua
@@ -0,0 +1,16 @@
+name = "Instant Assist"
+version = 2
+copyright = "MIT License"
+description = [[The mod allows engineers to instantly assist structures after the order has been given and they are within range,
+with no need to move around and to be dumb in general.
+
+Requires ReUI.Core >= 1.0.0 to work.
+]]
+author = "4z0t"
+url = "https://github.com/4z0t/FAF-UI-Mods"
+uid = "instant-assist-v02"
+exclusive = false
+ui_only = true
+conflicts = {}
+
+ReUI = "InstantAssist=1.1.0"
